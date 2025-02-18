4ade5ec1e45689d1998d549fcd0180be5039ae1e
hooliganny <83071435+hooliganny@users.noreply.github.com>
2025-02-18 15:12:49 +1300

    chore: using C# naming convention for namespace

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using ghoti_translator;
+using GhotiTranslator;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
