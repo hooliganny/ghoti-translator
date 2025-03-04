d37cac778eb97a930bed8e3b85c90d5224fb9a11
hooliganny <83071435+hooliganny@users.noreply.github.com>
2025-03-04 17:26:59 +1300

    feat: implement basic styles

--- wwwroot/js/main.js
+++ wwwroot/js/main.js
@@ -1,7 +1,5 @@
 CopyToClipboard = {
   copyToClipboard: function (text) {
-    // const translatedTextElement = document.getElementById("translated-text");
-    // const text = translatedTextElement.value;
     navigator.clipboard.writeText(text).then(
       function () {
         console.log("Async: Copying to clipboard was successful!");
